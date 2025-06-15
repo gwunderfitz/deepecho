672ef79a80ca03686690259f14dd86e71cb0a1fe
TAALAALM <alina.langmeier@swisscom.com>
2025-06-15 16:52:00 +0200

    audio

--- src/audio.js
+++ src/audio.js
@@ -1,33 +1,4 @@
-import { currentSong, currentPlaylist, currentIndex } from './stores/playback.js';
+// audio.js
+export const audio = new Audio('/music/Crystal_Skies.mp3');
 
-export const audio = new Audio();
-
-currentSong.subscribe((songName) => {
-  if (!songName) return;
-
-  const songModules = import.meta.glob('/public/music/*.mp3', { as: 'url' });
-  const allSongs = Object.entries(songModules).map(([path, url]) => ({
-    name: path.split('/').pop().split('.')[0],
-    url,
-  }));
-
-  const match = allSongs.find(s => s.name === songName);
-  if (match) {
-    audio.src = match.url;
-    audio.play();
-  }
-});
-
-audio.addEventListener('ended', () => {
-  let playlist;
-  let index;
-
-  currentPlaylist.subscribe(p => playlist = p)();
-  currentIndex.subscribe(i => index = i)();
-
-  if (playlist && playlist.length > 0) {
-    const nextIndex = (index + 1) % playlist.length;
-    currentIndex.set(nextIndex);
-    currentSong.set(playlist[nextIndex]);
-  }
-});
+audio.volume = 0.5;
